53e8a276f3ebc6be1c0360f9e8fa5ed4ba30f8e4
unknown <itsaustinchen@gmail.com>
2020-01-03 03:17:51 -0500

    Reorganize component files

--- web-app/src/components/SubHeader/index.js
+++ web-app/src/components/SubHeader/index.js
@@ -1,2 +1,24 @@
-import SubHeader from './subHeader'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { COLOURS } from '../../utils/constants'
+
+const style = {
+    fontSize: '2.3rem',
+    padding: '0.2rem 0 0.2rem 0',
+}
+
+const SubHeader = ({ colour, children, ...props }) => (
+    <h2 className={colour} style={{ ...style, ...props.style }}>
+        {children}
+    </h2>
+)
+
+SubHeader.propTypes = {
+    colour: PropTypes.oneOf(COLOURS),
+}
+
+SubHeader.defaultProps = {
+    colour: `black`,
+}
+
 export default SubHeader
