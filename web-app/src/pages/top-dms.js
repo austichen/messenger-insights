d64471123d884184cb4be2ea053c7fcb8188b164
unknown <itsaustinchen@gmail.com>
2020-01-07 02:30:15 -0500

    Add top dms page

--- web-app/src/pages/top-dms.js
+++ web-app/src/pages/top-dms.js
@@ -2,36 +2,46 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
-import { SubHeader } from '../components'
+import { SubHeader, GraphToggle } from '../components'
 import SEO from '../components/seo'
+import { groupPartitionedData } from '../utils/helpers'
 
 const TopDms = ({ data }) => {
     const { x, y } = data.stats.edges[0].node.topDms.data
-    const topDms = {}
-    const unpartitionedTopDms = {}
-    x.forEach((name, idx) => {
-        const { me, them } = y[idx]
-        topDms[name] = y[idx]
-        unpartitionedTopDms[name] = me + them
-    })
-
-    const topMessages = Object.entries(unpartitionedTopDms)
+    const topMessages = groupPartitionedData(y)
+    const topDmNames = x.map((chat, i) => [chat, y[i]])
         .sort((a, b) => a[1] - b[1])
         .map(v => v[0])
         .reverse()
-
+    
     return (
         <Layout pageNumber={5}>
             <SEO title="Page two" />
-            <SubHeader colour="grey">
-                One person had your attention in particular...
-            </SubHeader>
-            <h2>
-                You spent the most time talking to{' '}
-                <span className="purple">{topMessages[0]}</span>, with{' '}
-                <span className="yellow">{topMessages[1]}</span> and{' '}
-                <span className="blue">{topMessages[2]}</span> right behind
+            <h2 style={{marginTop: '20px'}}>
+                Throughout all these years, one person had your attention in particular...
             </h2>
+            <SubHeader colour="grey">
+                You spent the most time talking to{' '}
+                <span className="purple">{topDmNames[0]}</span>, with{' '}
+                <span className="yellow">{topDmNames[1]}</span> and{' '}
+                <span className="blue">{topDmNames[2]}</span> right behind
+            </SubHeader>
+            <GraphToggle
+                style={{marginTop: '30px'}}
+                type="bar"
+                x={x}
+                ys={[
+                    topMessages.total,
+                    {
+                        me: topMessages.me,
+                        them: topMessages.them,
+                    },
+                ]}
+                yToggleLabels={['Total', 'Partitioned']}
+                xLabel="Chat"
+                yLabel="Number of Messages"
+                title="Top DMs"
+            />
         </Layout>
     )
 }
