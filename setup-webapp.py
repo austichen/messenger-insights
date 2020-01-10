85ffac8e09d42ce1c259ed35738e4eab1589b4d9
unknown <itsaustinchen@gmail.com>
2020-01-09 19:54:48 -0500

    Dynamically query name and start year

--- setup-webapp.py
+++ setup-webapp.py
@@ -1,10 +1,31 @@
 import os
 import json
+import pandas as pd
 from visualize.visualize import *
 from utils.messages import get_start_end_years
+from utils.files import get_all_folders
+from utils.csv_utils import read_csv_in_folder
 
 START_YEAR, END_YEAR = get_start_end_years()
 stats_json_path = './web-app/src/data/stats/stats.json'
+
+def get_name():
+    folders = get_all_folders(dm=True, group_chat=False)
+    names = set()
+    i = 0
+    while len(names) != 2:
+        df = read_csv_in_folder(folders[i])
+        names = set(df.sender_name.unique())
+        i+=1
+    other_names = set()
+    while len(other_names) != 2:
+        df = read_csv_in_folder(folders[i])
+        other_names = df.sender_name.unique()
+        i += 1
+    for name in other_names:
+        if name in names:
+            return name
+    raise Exception('Unable to determine user\'s name')
 
 def main():
     # generate stats file
@@ -23,10 +44,14 @@ def main():
         },
         'topGcs': {
             'data': top_k_messages_all_time(partition_by_sender=True, group_chat=True, return_raw_data=True)
+        },
+        'metadata': {
+            'startYear': START_YEAR,
+            'endYear': END_YEAR,
+            'name': get_name()
         }
     }
 
-    print(stats_obj)
     try:
         os.makedirs(stats_json_path)
     except FileExistsError:
--- web-app/src/pages/index.js
+++ web-app/src/pages/index.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import { PageHeader, SubHeader } from '../components'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
     <Layout pageNumber={0}>
         <SEO title="Home" />
         <PageHeader colour="grey" style={{ paddingTop: '10%' }}>
-            Hi, Austin
+            Hi, {data.stats.edges[0].node.metadata.name.split(' ')[0]}
         </PageHeader>
         <SubHeader>
             You've been using <span className="blue">Messenger</span> since{' '}
-            <span className="teal">2010</span>.
+            <span className="teal">{data.stats.edges[0].node.metadata.startYear}</span>.
         </SubHeader>
         <SubHeader>Why don't we take a stroll down memory lane?</SubHeader>
         <SubHeader>
@@ -25,3 +26,18 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+    query {
+        stats: allStatsJson {
+            edges {
+                node {
+                    metadata {
+                        startYear
+                        name
+                    }
+                }
+            }
+        }
+    }
+`
